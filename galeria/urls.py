c985a7b6f2aeeabe7012f386a6fbdaa412f22ab2
Leandro Santos <leandros_cruz@outlook.com>
2023-03-31 11:47:57 -0300

    Ajustando os templates ajustando os templates para mostrar corretamente as informações do banco de dados solicitadas pelo usuário

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,5 +4,5 @@ from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'), ## recebendo um ID do objeto clicado no index, e passando como parametro para o view buscar e devolver o template da pagina com as informações corretas do DB
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 ## metodo nativo do DJANGO
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
-    return render(request, 'galeria\index.html', {"cards": fotografias})
+    fotografias = Fotografia.objects.all() ## puxando todos os objetos do DB
+    return render(request, 'galeria\index.html', {"cards": fotografias}) ## passando as informações puxadas para o navegador
 
-def imagem(request):
-    return render(request, 'galeria\imagem.html')
+def imagem(request, foto_id): ## recebendo ID que foi passado pelo urls e veio do index.html
+    fotografia = get_object_or_404(Fotografia, pk=foto_id) ## usando esse ID que é PK, para puxar as informações do DB
+    return render(request, 'galeria\imagem.html', {"fotografia": fotografia}) ## devolvendo as informações buscadas na DB para o navegador
